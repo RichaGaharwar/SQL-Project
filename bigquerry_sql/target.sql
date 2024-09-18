475e748f6d6c40964b2fc0c163fd1a73bdf504a8
Richa Gaharwar <gaharwarricha122@gmail.com>
2024-09-18 13:05:35 +0530

    Update target.sql

--- bigquerry_sql/target.sql
+++ bigquerry_sql/target.sql
@@ -1,9 +1,9 @@
--- Get the time range between which the orders were placed.
+-- The time range between which the orders were placed.
 
 select min(order_purchase_timestamp)as first_order, max(order_purchase_timestamp)as last_order from `target-sql-project-428910.target.orders`;
 
 
--- Count the Cities & States of customers who ordered during the given period
+-- Count of Cities & States of customers who ordered during the given period
 
 select count(distinct(geolocation_city))cities, count(distinct(geolocation_state))states from `target-sql-project-428910.target.geolocation`;
 
@@ -41,7 +41,7 @@ order by ordercount desc
 
 -- Evolution of E-commerce orders in the Brazil region:
  
--- Get the month on month no. of orders placed in each state
+-- The month on month no. of orders placed in each state
 
 select c.customer_state as states,
 extract(month from order_purchase_timestamp) as months, 
@@ -52,7 +52,7 @@ order by num_of_order desc
 limit 10
 
 
--- How are the customers distributed across all the states
+-- The customers distributed across all the states
 
 select customer_state, count(*) no_of_customers
 from `target-sql-project-428910.target.customers`
@@ -60,10 +60,7 @@ group by customer_state
 order by no_of_customers desc
 limit 10
 
--- Impact on Economy: Analyze the money movement by e-commerce by looking at order prices, freight and others.
- 
--- Get the % increase in the cost of orders from year 2017 to 2018(include months between Jan to Aug only.
--- You can use the "payment_value" column in the payments table to get the cost of orders)
+-- Impact on Economy: Analysis of the money movement by e-commerce, looking at order prices, freight and others.
 
 WITH orders_2017 AS (
  SELECT o.order_id, p.payment_value
@@ -86,7 +83,7 @@ WHERE orders_2017.order_id = orders_2018.order_id
 
 
 
--- Calculate the Total & Average value of order price for each state.
+--The Total & Average value of order price for each state.
 
 select distinct c.customer_state as state, round(sum(p.payment_value),2) as total_value, round(avg(p.payment_value),2) as avg_value 
 from `target-sql-project-428910.target.payments` p inner join `target-sql-project-428910.target.orders` o on p.order_id = o.order_id
@@ -96,7 +93,7 @@ order by total_value desc, avg_value desc
 limit 10;
 
 
--- Calculate the Total & Average value of order freight for each state.
+--The Total & Average value of order freight for each state.
 
 select c.customer_state as state, round(sum(oi.freight_value),2) total_value, round(avg(oi.freight_value),2) avg_value
 from `target-sql-project-428910.target.order_items` oi inner join `target-sql-project-428910.target.orders` o on oi.order_id = o.order_id
@@ -106,8 +103,8 @@ order by total_value desc
 limit 10;
 
 
--- Find the no. of days taken to deliver each order from the order’s purchase date as delivery time.
--- Also, calculate the difference (in days) between the estimated & actual delivery date of an order.
+-- The no. of days taken to deliver each order from the order’s purchase date as delivery time.
+-- Also, the difference (in days) between the estimated & actual delivery date of an order.
 
 -- Do this in a single query.You can calculate the delivery time and the difference between the estimated & actual delivery date using the given formula:
 -- [time_to_deliver = order_delivered_customer_date-order_purchase_timestamp]
@@ -118,7 +115,7 @@ date_diff(order_estimated_delivery_date, order_delivered_customer_date,day) as d
 from `target-sql-project-428910.target.orders` 
 
 
--- Find out the top 5 states with the highest & lowest average freight value
+--Tthe top 5 states with the highest & lowest average freight value
 
 select c.customer_state as state,round(avg(oi.freight_value),2) avg_value
 from `target-sql-project-428910.target.order_items` oi inner join `target-sql-project-428910.target.orders` o on oi.order_id = o.order_id
@@ -127,16 +124,8 @@ group by state
 order by avg_value desc
 limit 5;
 
-select c.customer_state as state,round(avg(oi.freight_value),2) avg_value
-from `target-sql-project-428910.target.order_items` oi inner join `target-sql-project-428910.target.orders` o on oi.order_id = o.order_id
-inner join `target-sql-project-428910.target.customers` c on o.customer_id = c.customer_id
-group by state
-order by avg_value asc
-limit 5;
 
-
-
--- Find out the top 5 states with the highest & lowest average delivery time
+--The top 5 states with the highest & lowest average delivery time
 
 select c.customer_state as state, round(avg(extract(second from o.order_delivered_customer_date)- extract(second from o.order_purchase_timestamp)/86400),2) as avg_time_to_deliver
 from `target-sql-project-428910.target.orders` o join `target-sql-project-428910.target.customers` c on o.customer_id = c.customer_id
@@ -152,9 +141,7 @@ limit 5
 
 
 
--- Find out the top 5 states where the order delivery is really fast as compared to the estimated date of delivery.
-
--- You can use the difference between the averages of actual & estimated delivery date to figure out how fast the delivery was for each state
+--The top 5 states where the order delivery is really fast as compared to the estimated date of delivery.
 
 select c.customer_state as states, 
 avg(o.order_delivered_customer_date - o.order_purchase_timestamp) as avg_delivery_time
@@ -165,7 +152,7 @@ order by avg_delivery_time asc
 limit 5;
 
  
--- Find the month on month no. of orders placed using different payment types
+--The month on month no. of orders placed using different payment types
  
 select extract(year from o.order_purchase_timestamp)as years, 
 extract(month from o.order_purchase_timestamp)as months, 
@@ -179,7 +166,7 @@ order by years, months
 limit 10
 
 
--- Find the no. of orders placed on the basis of the payment installments that have been paid
+-- The no. of orders placed on the basis of the payment installments that have been paid
 
 select payment_installments, count(*) as order_count
 from `target-sql-project-428910.target.payments`
